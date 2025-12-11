27850fa5e3696fc7061526a072cbb238064485fa
Shannon Lin <Sl3454@cornell.edu>
2025-12-11 14:16:53 -0500

    Added scenario 1 page

--- app.js
+++
@@ -1,4 +0,0 @@
-function goToPage(pageNumber) {
-    document.querySelectorAll('.page').forEach(p => p.style.display = 'none');
-    document.getElementById('page' + pageNumber).style.display = 'block';
-}
