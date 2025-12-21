72e7a83a72abf28d18a3fb9ef19e1fdb7b03e53c
Shannon Lin <Sl3454@cornell.edu>
2025-12-21 00:37:04 -0700

    SECRET MESSAGE HEHE

--- analytics/count.js
+++ analytics/count.js
@@ -8,7 +8,12 @@ import { db } from "../js/firebase-backend.js";
 async function countDocs() {
   const coll = collection(db, "plays");
   const snapshot = await getCountFromServer(coll);
-  console.log("🧀 Total cheese finders:", snapshot.data().count);
+  console.log(
+  "%c🐀 CURSE ACTIVATED 🧀\n\nBy opening this console,\nyou have invoked the ancient dairy spell.\n\nYou are now:\n→ a lactose-intolerant rat\n→ haunted by brie\n→ unable to enjoy pizza without consequences\n\nThere is no undo.\nRespectfully.",
+  "color: #7A1F1F; font-size: 14px; font-weight: bold; font-family: monospace;"
+);
+
+  console.log("But if you're curious (+ make you feel better), this is how many people have taken the cheese quiz: ", snapshot.data().count);
 }
 
 countDocs();
